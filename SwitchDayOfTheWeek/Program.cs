15c0102b4c298ad6d4eaa6437b537e873fe2e382
Khan <84818798+Khan-dev-hash@users.noreply.github.com>
2022-05-11 23:33:34 -0500

    Add project files.

---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("What day of the week is it? ");
+
+        string dayOfTheWeek = Console.ReadLine();
+
+        switch (dayOfTheWeek)
+        {
+            case "Monday":
+                Console.WriteLine("Just another manic Monday...I wish it were Sunday");
+                break;
+            case "Tuesday":
+                Console.WriteLine("Taco Tuesday!!!");
+                break;
+            case "Wednesday":
+                Console.WriteLine("Humpday");
+                break;
+            case "Thursday":
+                Console.WriteLine("Throwback Thursday");
+                break;
+            case "Friday":
+                Console.WriteLine("TGIF");
+                break;
+            case "Saturday":
+                Console.WriteLine("Surreal Saturday");
+                break;
+            case "Sunday":
+                Console.WriteLine("Get ready for Monday!");
+                break;
+
+            default:
+                Console.WriteLine("Enter a valid input");
+                break;
+
+        }
+    }
+}
